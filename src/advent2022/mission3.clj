60c1b66720e324c7849d12661268c5db9abf5e91
Martin Collberg <martin.collberg@veoneer.com>
2022-12-04 21:00:03 +0100

    Before attempting real input on mission 3

--- src/advent2022/mission3.clj
+++ src/advent2022/mission3.clj
@@ -1,23 +1,32 @@
 (ns advent2022.mission3
   (:require [clojure.java.io :as io]
-            [utils.utils :as utils])
+            [utils.utils :as utils]
+            [clojure.set :as set])
   (:gen-class))
 
-(def rucksack {:comparments
-               {:big1 nil
-                :big2 nil}})
-
-(def bag1 "vJrwpWtwJgWrhcsFMMfFFhFp")
 
 (defn parse-line
-  "returns a bag data structure from the input line"
+  "returns a rucksack data structure from the input line"
   [line]
   (let [[comp1 comp2] (split-at (/ (count line) 2) line)]
-    { :comp1 comp1
+    { :comp1 comp1 ; first compartment in the rucksack
      :comp2 comp2
+     :common (first (set/intersection (set comp1) (set comp2)))
      }))
 
-(parse-line bag1)
+;;Lowercase item types a through z have priorities 1 through 26.
+;;Uppercase item types A through Z have priorities 27 through 52.
+(defn prio
+  [in]
+  (if (>= (int in) (int \a))
+    (inc  (- (int in) (int \a)))
+    (+ 27 (- (int in) (int \A)))))
+
+(->> (utils/get-lines "resources/3_input.txt")
+     (map parse-line)
+     (map :common)
+     (map prio)
+     (reduce +))
 
 ;; Every item type is identified by a single lowercase or uppercase
 ;; letter (that is, a and A refer to different types of items).
