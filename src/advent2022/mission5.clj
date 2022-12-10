26614a6c67b368033405245ce03b05513408dcaa
Martin Collberg <martin.collberg@veoneer.com>
2022-12-10 11:51:02 +0100

    Working on tail-recursive parsing of crate-input

--- src/advent2022/mission5.clj
+++ src/advent2022/mission5.clj
@@ -1,12 +1,1 @@
-(ns advent2022.mission5
-  (:require [clojure.java.io :as io]
-            [utils.utils :as utils]
-            [clojure.set :as set])
-  (:gen-class))
-
-(defn parse-line 
-  [input]
-  (let [matches (re-matches #"(\d+)-(\d+),(\d+)-(\d+)", input)
-        matches-int (map utils/as-integer (drop 1 matches)) 
-        [a1 a2 b1 b2] matches-int]
-    nil))
+;;(let [d (take 3 data)]
