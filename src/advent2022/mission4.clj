d9b18269fb445760890e36264701846c8354ff03
Martin Collberg <martin.collberg@veoneer.com>
2022-12-10 11:24:05 +0100

    Complete part2 of day4

--- src/advent2022/mission4.clj
+++ src/advent2022/mission4.clj
@@ -9,7 +9,6 @@
   [from to]
   (range (int from) (int (inc to))))
 
-
 (defn parse-line 
   [input]
   (let [matches (re-matches #"(\d+)-(\d+),(\d+)-(\d+)", input)
@@ -24,6 +23,11 @@
     a
     b))
 
+(defn into-sets
+  [data]
+  {:a (set (:a 
+            (parse-line data)))
+   :b (set (:b (parse-line data)))})
 
 (defn fully-contains
   [a b]
@@ -31,13 +35,32 @@
    (into (hash-set) (set/union a b))
    (into (hash-set) (max-count a b))))
 
-;; In how many assignment pairs does one range fully contain the other?
-(->>
- (map #(fully-contains (:a %) (:b %))
-      (->>
-       (utils/get-lines "resources/4_input.txt")
-       (map parse-line)))
- 
- (filter #(= % true))
- count)
+(defn overlap-at-all
+  [a b]
+  (empty? (set/intersection (into (hash-set) a)
+                            (into (hash-set) b))))
 
+;; In how many assignment pairs does one range fully contain the other?
+
+;; (let [data (parse-line "5-7,7-9")
+;;       a (:a data)
+;;       b (:b data)]
+;;   (empty? (set/intersection (into (hash-set) a)
+;;                             (into (hash-set) b))))
+
+
+(->> (map #(fully-contains (:a %) (:b %))
+          (->>
+           (utils/get-lines "resources/4_input_full.txt")
+           (map parse-line)))
+     (filter #(= % true))
+     count)
+
+;; Part 2
+;; In how many assignment pairs does one range fully overlap-at-all?
+(->> (map #(overlap-at-all (:a %) (:b %))
+          (->>
+           (utils/get-lines "resources/4_input.txt")
+           (map parse-line)))
+     (filter #(= % true))
+     count)
