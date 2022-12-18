f380576d2b0458508da8178398af0af5f5a3b320
Martin Collberg <martin.collberg@veoneer.com>
2022-12-18 14:13:42 +0100

    prep for mission 7

---
+++ src/advent2022/mission7.clj
@@ -0,0 +1,12 @@
+(ns advent2022.mission7
+  (:require [clojure.java.io :as io]
+            [utils.utils :as utils]
+            [clojure.string :as string]
+            [clojure.set :as set])
+  (:gen-class))
+
+
+
+;; Part 1 -> 1582
+(utils/get-lines "resources/7_input.txt")
+
