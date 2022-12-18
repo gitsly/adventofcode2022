ed33ebdb53cbf17790e51638e1ef5720dd907ab2
Martin Collberg <martin.collberg@veoneer.com>
2022-12-18 17:39:13 +0100

    Mission7: start feeding input

--- src/advent2022/mission7.clj
+++ src/advent2022/mission7.clj
@@ -5,8 +5,35 @@
             [clojure.set :as set])
   (:gen-class))
 
+(comment "
+- / (dir)
+  - a (dir)
+    - e (dir)
+      - i (file, size=584)
+    - f (file, size=29116)
+    - g (file, size=2557)
+    - h.lst (file, size=62596)
+  - b.txt (file, size=14848514)
+  - c.dat (file, size=8504156)
+  - d (dir)
+    - j (file, size=4060174)
+    - d.log (file, size=8033020)
+    - d.ext (file, size=5626152)
+    - k (file, size=7214296)")
 
+(def commands { :cd #"cd (.*)" })
+
+
+(def sample-dir {:total-size 12
+                 :wd "/"})
 
 ;; Part 1 -> 1582
-(utils/get-lines "resources/7_input.txt")
+(let [all-lines (utils/get-lines "resources/7_input.txt")]
+  (loop [lines all-lines]
+    (println (first lines))
+    (if(empty? lines)
+      nil
+      (recur (rest lines)))))
 
+
+(utils/get-lines "resources/7_input.txt")
