c05f552a02fd4af4f1ed0ad7c11296d704c23a12
Martin Collberg <martin.collberg@veoneer.com>
2023-01-04 13:09:36 +0100

    Seems like my root already is a tree-seq

--- src/advent2022/trees.clj
+++ src/advent2022/trees.clj
@@ -2,5 +2,27 @@
   (:require [clojure.java.io :as io]
             [utils.utils :as utils]
             [clojure.string :as string]
-            [clojure.set :as set])
+            [clojure.set :as set]
+            [clojure.walk :as walk])
   (:gen-class))
+
+
+(defn branch?
+  "Return true if node 'can' contain children"
+  [node]
+  (not (nil? (:children node))))
+
+(defn children
+  "Returns vector of children for passed 'node'"
+  [node]
+  (:children node))
+
+(def root {:children [{:children []
+                       :name "child1"}
+                      {:children []
+                       :name "child2"}]
+           :name "testroot"})
+
+(tree-seq branch? children root)
+
+(walk/prewalk-demo root)
