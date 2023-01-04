e418d5eccad668916bdc184eabf461c5ca6fd41c
Martin Collberg <martin.collberg@veoneer.com>
2023-01-04 13:48:34 +0100

    Show sample on how to use tree-seq successfully, works on map

--- src/advent2022/trees.clj
+++ src/advent2022/trees.clj
@@ -25,4 +25,21 @@
 
 (tree-seq branch? children root)
 
-(walk/prewalk-demo root)
+;; Simple test
+(def mymap {:hello 5 :test {:hello 2}})
+;;You can get the lazy sequence of its nodes like so:
+(def tree-lazy-seq
+  (tree-seq map? vals mymap))
+;;=> ({:hello 5, :test {:hello 2}} ;root node
+;;    5                            ;first leaf
+;;    {:hello 2}                   ;second child
+;;    2)                           ;child's leaf
+
+
+(walk/prewalk-demo tree-lazy-seq)
+
+;; Below is same as above
+(walk/prewalk
+ (fn [node]
+   (println node)
+   node) tree-lazy-seq)
