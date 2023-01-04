29e012a496ba13b5970d3658a305117254d56e52
Martin Collberg <martin.collberg@veoneer.com>
2023-01-04 12:57:16 +0100

    Add trees.clj for experimenting with tree structures, going to test pre-walk etc

---
+++ src/advent2022/trees.clj
@@ -0,0 +1,6 @@
+(ns advent2022.trees
+  (:require [clojure.java.io :as io]
+            [utils.utils :as utils]
+            [clojure.string :as string]
+            [clojure.set :as set])
+  (:gen-class))
