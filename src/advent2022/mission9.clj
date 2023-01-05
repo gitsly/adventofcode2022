50b7fddac9709e511265151e2e35db9574e6b153
Martin Collberg <martin.collberg@veoneer.com>
2023-01-05 17:26:10 +0100

    working on move fn

--- src/advent2022/mission9.clj
+++ src/advent2022/mission9.clj
@@ -9,19 +9,49 @@
 
 ;; Moves
 
-(defn vdiff
+(defn vsub
   [a b]
   (vec
    (map - a b)))
 
-(let [s [0, 0]
-      H s
-      T [1, 1]
-      diff (vdiff H T)
+(defn vadd
+  [a b]
+  (vec
+   (map + a b)))
 
-      move :U]
+(defn vlen
+  "Calculates length of vector"
+  [v]
+  (Math/sqrt (+ (exp (v 0) 2)
+                (exp (v 1) 2))))
 
-  )  
+(def diag-len (vlen [1 1])) ; Length of a diagonal
+
+
+(defn exp [x n]
+  (reduce * (repeat n x)))
+
+
+(let [H [0, 0]
+      T [0, 0]
+
+      tail-fn (fn
+                [H T]
+                (let [len (vlen (vsub H T))]
+                  (cond
+                    (> len diag-len) {:len len}
+                    :else T)))
+
+      move :D
+      moves {:U [0 -1]
+             :D [0  1]
+             :R [1  0]
+             :L [-1 0]}]
+
+  (let [H-next (vadd H (move moves))
+        diff (vsub H-next T)] 
+    {:H H-next
+     :T (tail-fn H-next T) }))  
 
 
 ;; Solution
