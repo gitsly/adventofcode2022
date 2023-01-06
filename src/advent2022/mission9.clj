87a5052ca6bb8766597a77b77cd6d27e6e43c73b
Martin Collberg <martin.collberg@veoneer.com>
2023-01-06 14:22:39 +0100

    Intrusive: figuring stuff out

--- src/advent2022/mission9.clj
+++ src/advent2022/mission9.clj
@@ -24,50 +24,62 @@
   (Math/sqrt (+ (exp (v 0) 2)
                 (exp (v 1) 2))))
 
+(reduce (fn[a
+            b]
+          (println a b)
 
+          (vadd 
+           a
+           b))
+
+        [[1 0]
+         [0 2]
+         [1 2]])
+
+
+;; maybe use reduce? 
 (defn apply-with-prev
-  "applies fn taking two parameters with prev item in coll for every item in coll"
-  [fn
-   coll]
-  (loop [c coll
-         prev nil
-         res []]
-    (if (empty? c)
-      res
-      (recur (rest c)
-             (first c)
-             (conj res (fn (first c) prev))))))
+"applies fn taking two parameters with prev item in coll for every item in coll"
+[fn
+ coll]
+(loop [c coll
+       prev nil
+       res []]
+  (if (empty? c)
+    res
+    (recur (rest c)
+           (first c)
+           (conj res (fn (first c) prev))))))
 
 
 
 (defn exp [x n]
   (reduce * (repeat n x)))
 
+(def motions {:U [0 -1]
+              :D [0  1]
+              :R [1  0]
+              :L [-1 0]})
+
+
 (defn move
   [H ; 2d vec (head)
    T ; 2D vec (tail)
-   move-id]
-  (let [
-        tail-fn (fn
-                  [H T move]
-                  (cond
-                    (> (vlen (vsub H T)) diag-len) (let [T-next (vadd T move)]
-                                                     (cond
-                                                       (> (abs (move 0)) 0) [(T-next 0) (H 1)] ; horiz move (ensure same y)
-                                                       ;; else: vertical move (ensure same x)
-                                                       :else [(H 0) (T-next 1)]) 
-                                                     )
-                    :else T)) ; No need to move Tail.
-        diag-len (vlen [1 1]) ; Length of a diagonal
-        moves {:U [0 -1]
-               :D [0  1]
-               :R [1  0]
-               :L [-1 0]}]
+   v]
+  (let [diag-len (vlen [1 1]) ; Length of a diagonal
+        H (vadd H v); update H with vector
+        ]
+    (cond
+      (> (vlen (vsub H T)) diag-len) (let [T-next (vadd T v)]
+                                       (cond
+                                         (> (abs (v 0)) 0) [(T-next 0) (H 1)] ; horiz move (ensure same y)
+                                         ;; else: vertical move (ensure same x)
+                                         :else [(H 0) (T-next 1)]))
+      :else T; No need to move Tail.
+      ))) 
 
-    (let [H-next (vadd H (move-id moves))
-          T-next (tail-fn H-next T (move-id moves))] 
-      [H-next T-next] )))
 
+(move [2 -1] [1 0] [0 -1]) ; Hmm, if moving, tail seems to always end up in previous Head position.
 
 
 ;; Solution
@@ -77,17 +89,22 @@
                    (let [[_ cmd cnt] (re-matches #"(.*) (\d*)" line)]
                      (repeat  (utils/as-integer cnt) (keyword cmd))))
 
-      all-moves (flatten (map parse-line lines))
+      all-moves (map motions (flatten (map parse-line lines)))
 
-      all-moves [:R]
+      all-moves (map motions [:R])
 
       move-all (fn [state
                     m]
-                 ;; (move state m)
                  (update state :knots 
                          #(apply-with-prev (fn[tail
                                                head]
-                                             (println "Head:" head "Tail:" tail))
+                                             (println "Head:" head "Tail:" tail)
+                                             (if (nil? head)
+                                               :head
+                                               ((move head tail m) 1))
+
+                                             )
+
                                            %))
 
                  )
@@ -107,10 +124,4 @@
                               (:T-history final-state))
       ]
 
-  final-state)
-
-(let [test  [1 2 3 4]]
-(apply-with-prev (fn[i prev]
-                   {:i i
-                    :prev prev })
-                 test))
+  all-moves )
