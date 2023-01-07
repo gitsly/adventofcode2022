3e66aa68fe8fbfd91f9923781736959f3e82ccd1
Martin Collberg <martin.collberg@veoneer.com>
2023-01-07 11:35:02 +0100

    Define T vector from center where H is center

--- src/advent2022/mission9moves.clj
+++ src/advent2022/mission9moves.clj
@@ -5,8 +5,143 @@
             [clojure.set :as set]
             [clojure.walk :as walk])
   (:gen-class))
+
+;; kan vi köpa 'E' med mixad kvalite
+
+(reduce (map -) [[0 0 ] [0 0 ]] ) 
+
+
+[{:org [0 -2] ; org is from H (centre 0 0) to T
+  :vec [0 -1] ; vector needed to move 'T' inorder to follow 'H'
+  :comment "
+..T..
+.....
+..H..
+.....
+....."}
+ {:org [1 -2] ; org is from H (centre 0 0) to T
+  :vec [0 0] ; vector needed to move 'T' inorder to follow 'H'
+  :comment "
+...T.
+.....
+..H..
+.....
+....."}
+ {:org [-2 -2] ; org is from H (centre 0 0) to T
+  :vec [0 0] ; vector needed to move 'T' inorder to follow 'H'
+  :comment "
+....T
+.....
+..H..
+.....
+....."}
+ {:org [2 -1] ; org is from H (centre 0 0) to T
+  :vec [0 0] ; vector needed to move 'T' inorder to follow 'H'
+  :comment "
+.....
+....T
+..H..
+.....
+....."}
+ {:org [2 0] ; org is from H (centre 0 0) to T
+  :vec [0 0] ; vector needed to move 'T' inorder to follow 'H'
+  :comment "
+.....
+.....
+..H.T
+.....
+....."}
+ {:org [2 1] ; org is from H (centre 0 0) to T
+  :vec [0 0] ; vector needed to move 'T' inorder to follow 'H'
+  :comment "
+.....
+.....
+..H..
+....T
+....."}
+ {:org [2 2] ; org is from H (centre 0 0) to T
+  :vec [0 0] ; vector needed to move 'T' inorder to follow 'H'
+  :comment "
 .....
 .....
 ..H..
 .....
+....T"}
+ {:org [1 2] ; org is from H (centre 0 0) to T
+  :vec [0 0] ; vector needed to move 'T' inorder to follow 'H'
+  :comment "
 .....
+.....
+..H..
+.....
+...T."}
+ {:org [0 2] ; org is from H (centre 0 0) to T
+  :vec [0 0] ; vector needed to move 'T' inorder to follow 'H'
+  :comment "
+.....
+.....
+..H..
+.....
+..T.."}
+ {:org [-1 2] ; org is from H (centre 0 0) to T
+  :vec [0 0] ; vector needed to move 'T' inorder to follow 'H'
+  :comment "
+.....
+.....
+..H..
+.....
+.T..."}
+ {:org [-2 2] ; org is from H (centre 0 0) to T
+  :vec [0 0] ; vector needed to move 'T' inorder to follow 'H'
+  :comment "
+.....
+.....
+..H..
+.....
+T...."}
+
+
+
+ {:org [-2 1] ; org is from H (centre 0 0) to T
+  :vec [0 0] ; vector needed to move 'T' inorder to follow 'H'
+  :comment "
+.....
+.....
+..H..
+T....
+....."}
+ {:org [-2 0] ; org is from H (centre 0 0) to T
+  :vec [0 0] ; vector needed to move 'T' inorder to follow 'H'
+  :comment "
+.....
+.....
+T.H..
+.....
+....."}
+ {:org [-2 -1] ; org is from H (centre 0 0) to T
+  :vec [0 0] ; vector needed to move 'T' inorder to follow 'H'
+  :comment "
+.....
+T....
+..H..
+.....
+....."}
+ {:org [-2 -2] ; org is from H (centre 0 0) to T
+  :vec [0 0] ; vector needed to move 'T' inorder to follow 'H'
+  :comment "
+T....
+.....
+..H..
+.....
+....."}
+ {:org [-1 -2] ; org is from H (centre 0 0) to T
+  :vec [0 0] ; vector needed to move 'T' inorder to follow 'H'
+  :comment "
+.T...
+.....
+..H..
+.....
+....."}]
+
+
+
