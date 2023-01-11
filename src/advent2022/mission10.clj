2817394c07796611c6069e5a30a8545c33af8ead
Martin Collberg <martin.collberg@veoneer.com>
2023-01-11 21:15:25 +0100

    start parsing input

--- src/advent2022/mission10.clj
+++ src/advent2022/mission10.clj
@@ -6,7 +6,28 @@
             [clojure.walk :as walk])
   (:gen-class))
 
-(utils/get-lines "resources/input_10.txt")
+
+(let [parse-line (fn[line]
+                   (let [[_ op v] (re-matches #"(.*) (\d*)" line)]
+                     (cond v {:addx (utils/as-integer v)})))]
+  (map parse-line
+       (utils/get-lines "resources/input_10.txt")
+       ))
+
+
+(let [initial-cpu {:x 1
+                   :op-cnt 0}
+      ]
+  
+  (loop [cpu initial-cpu
+         instr ]))
+
+
+
+;; signal Strength = the cycle number multiplied by the value of the X register.
+
+;; Check signal-strength every 20nth cycle:  20th, 60th, 100th, 140th, 180th, and 220
+
 
 (comment "
 The CPU has a single register, X, which starts with the value 1. It supports only two instructions:
