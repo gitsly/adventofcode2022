587424e02a169778896e6ee5edc3a681ce1c1a70
Martin Collberg <martin.collberg@veoneer.com>
2023-01-12 09:54:51 +0100

    This might work

--- src/advent2022/mission10.clj
+++ src/advent2022/mission10.clj
@@ -59,53 +59,15 @@
                                   (-> (cond
                                         (nil? op) (noop-fn cpu)
                                         (:addx op) (addx-fn cpu)
-                                        :else (-> cpu ; noop
+                                        :else (-> cpu ; noop (or no more instructions)
                                                   (update :ops #(vec (rest %)))))
                                       (update :cycle inc))))] 
 
                    (println "do-cycle" cpu)
-                   (lazy-seq
-                    (if (empty? (:ops cpu)) 
-                      cpu
-                      (cons cpu
-                            (do-cycle (cpu-fn cpu)))))))
+                   (lazy-seq (cons cpu (do-cycle (cpu-fn cpu))))))
 
       ]
   
-  (map #(dissoc % :ops) (do-cycle initial-cpu))
-
-  ;;(addx-fn {:x 1, :cycle 1, :ops [{:addx 23, :op-cycles 1}]})
-
-  )
-
-(defn positive-numbers 
-  ([] (positive-numbers 1))
-  ([n] (lazy-seq (cons n (positive-numbers (inc n))))))
-
-(take 3 (positive-numbers))
-
-(defn fib
-  ([] (fib 1 1))
-  ([a b] (lazy-seq
-          (cons a (fib (+ a b) a)))))
-
-(take 10 (fib))
-
-(defn do-cycle
-  [cpu]
-  (println "loop" cpu)
-  (let [cycle-fn (fn[cpu]
-                   (-> cpu
-                       (update :ops #(vec (rest %)))
-                       (update  :cycle inc)))]
-    (lazy-seq (cons cpu (do-cycle (cycle-fn cpu))))))
-
-
-(take-while #(not (empty? (:ops %)))
-            (do-cycle {:note "some state" :cycle 0 :ops [1 2 3]}))
-
-
-(distinct
- (map :done
-      (take 130 (do-cycle {:note "state1"}))))
-
+  (map #(dissoc % :ops) 
+       ;; (take-while #(not (empty? (:ops %))) (do-cycle initial-cpu))
+       (take 7 (do-cycle initial-cpu))))
