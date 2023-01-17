7d77c2ead733f5bdcfdc0cafe098aa1ce9704d73
Martin Collberg <martin.collberg@veoneer.com>
2023-01-17 06:36:23 +0100

    make fun

--- src/advent2022/mission11.clj
+++ src/advent2022/mission11.clj
@@ -14,33 +14,34 @@
                                      (utils/get-lines "resources/input_11.txt")))
 
       parse-raw-monkey (fn[raw-monkey]
-                         raw-monkey)
+                         (let [[id-line
+                                items-line
+                                operation-line
+                                test-line
+                                test-line-true
+                                test-line-false] raw-monkey
+                               [_ id] (re-matches #"Monkey (.*):" id-line)
+                               [_ items] (re-matches #"\s*Starting items: (.*)" items-line)
+                               [_ op arg] (re-matches #"\s*Operation: new = old (.*) (.*)" operation-line)
+                               [_ div] (re-matches #"\s*Test: divisible by (.*)" test-line)
+                               [_ true-target] (re-matches #"\s*If .*: throw to monkey (.*)" test-line-true)
+                               [_ false-target] (re-matches #"\s*If .*: throw to monkey (.*)" test-line-false)
+                               
+                               ]
+
+                           {:id (utils/as-integer id)
+                            :items (vec (utils/parse-int-list items))
+                            :divisable (utils/as-integer div)
+                            :true-target (utils/as-integer true-target)
+                            :false-target (utils/as-integer false-target)
+                            :op {(keyword op) (utils/as-integer arg)}})
+                         )
 
       ]
   ;; (map println (parse-raw-monkey  test-monkey-raw))
 
-  (let [monkey-lines test-monkey-raw
+  (parse-raw-monkey  test-monkey-raw)
 
-        [id-line
-         items-line
-         operation-line
-         test-line
-         test-line-true
-         test-line-false] monkey-lines
-        [_ id] (re-matches #"Monkey (.*):" id-line)
-        [_ items] (re-matches #"\s*Starting items: (.*)" items-line)
-        [_ op arg] (re-matches #"\s*Operation: new = old (.*) (.*)" operation-line)
-        [_ div] (re-matches #"\s*Test: divisible by (.*)" test-line)
-        [_ true-target] (re-matches #"\s*If .*: throw to monkey (.*)" test-line-true)
-        [_ false-target] (re-matches #"\s*If .*: throw to monkey (.*)" test-line-false)
-        
-        ]
-
-    {:id (utils/as-integer id)
-     :items (vec (utils/parse-int-list items))
-     :divisable (utils/as-integer div)
-     :true-target (utils/as-integer true-target)
-     :false-target (utils/as-integer false-target)
-     :op {(keyword op) (utils/as-integer arg)}}))
+  )
 
 
